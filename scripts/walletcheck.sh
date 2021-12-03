e7a49dbddc7a0a215a60190740ec3b82e3551fb1
JohanTheDeveloper <jpwdevries@gmail.com>
2021-12-03 22:27:13 +0100

    Swapping wallet.dat

--- scripts/walletcheck.sh
+++ scripts/walletcheck.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if [ ! -f "/root/.vericonomy/vericoin/walletnew.dat" ]; then
-    echo "/root/.vericonomy/vericoin/walletnew.dat not exist"
+if [ -f "/root/.vericonomy/vericoin/walletnew.dat" ]; then
+    echo "/root/.vericonomy/vericoin/walletnew.dat does exist. Starting swapping."
 
     mv /root/.vericonomy/vericoin/wallet.dat /root/.vericonomy/vericoin/wallet_$(date +%F).dat
     mv /root/.vericonomy/vericoin/walletnew.dat /root/.vericonomy/vericoin/wallet.dat
+
+    echo "Swapping completed. Old wallet.dat renamed to wallet_$(date +%F).dat"
 fi
